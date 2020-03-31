9bdecd52a4d7690dc78962ee48295bf807a678d1
seraphinatarrant <serif@uw.edu>
2020-03-31 17:03:22 +0100

    preprocessing code to create opensubs monolingual data

---
+++ preprocess/open_subs_preprocess.py
@@ -0,0 +1,133 @@
+#for each lang pair
+#    get overlap
+#load in zip files
+#extract overlapping files and make into format like dir: en-fr with subdir en and fr.
+
+import argparse
+import glob
+import gzip
+
+import os
+import pickle
+
+import tqdm
+import zipfile
+import xml.etree.ElementTree as ET
+
+
+from collections import defaultdict
+
+
+def setup_argparse():
+    p = argparse.ArgumentParser()
+    p.add_argument('--s', dest='source_dir', help='source dir for files')
+    p.add_argument('--t', dest='target_dir', help='target dir to write extracted text')
+    return p.parse_args()
+
+
+def find_overlapping_files(path='', all_files=False, target_dir=''):
+    if all_files:
+        files = glob.glob(path + '*.xml.gz')
+    else:
+        files = glob.glob(path + '.xml.gz') # this will be lang1-lang2.xml.gz
+    dic = defaultdict(dict)
+    # 1. Loop over all ces files in path (e.g. de-en.xml.gz) to find overlapping files.
+    # 2. Create a dictionary containing matching filenames
+    # example dictionary output:
+    # {
+    #   'en/1921/12349/5775558.xml.gz': {
+    #       'de': 'de/1921/12349/5510195.xml.gz',
+    #       'es': 'es/1921/12349/4260047.xml.gz',
+    #       'et': 'et/1921/12349/5184442.xml.gz',
+    #       'fi': 'fi/1921/12349/3676439.xml.gz',
+    #       'nl': 'nl/1921/12349/3683372.xml.gz',
+    #       'sv': 'sv/1921/12349/5510194.xml.gz'
+    #   }
+    # }
+    print('Found files {}'.format(files))
+    for i, f in enumerate(files):
+        print('Processing {} | {}/{}'.format(f, i + 1, len(files)))
+        with gzip.open(f, 'rt') as fl:
+            try:
+                tree = ET.parse(fl)
+            except OSError:
+                print('skipping')
+                # os.remove(f)
+                continue
+            root = tree.getroot()
+            linkGrps = root.findall('linkGrp')
+            for linkGrp in tqdm(linkGrps, desc=f,
+                                bar_format='{desc}{percentage:3.0f}%|{bar}|{n_fmt}/{total_fmt}'):
+                # check for .gz ending and strip it
+                fromDoc, f_ext = os.path.splitext(linkGrp.get('fromDoc'))
+                toDoc, t_ext = os.path.splitext(linkGrp.get('toDoc'))
+
+                # fromDoc is always english
+                default_from = 'en'
+                tmp = toDoc
+                toDoc = toDoc if not toDoc.startswith(default_from) else fromDoc
+                fromDoc = fromDoc if fromDoc.startswith(default_from) else tmp
+                dic[fromDoc].update({toDoc[:2]: toDoc})
+
+    # 3. Save the dictionary in a file
+    out_file = os.path.join(target_dir, '{}_file_ids'.format(path)) if not all_files else 'all_file_ids'
+    with open(out_file, 'wb') as f:
+        pickle.dump(dic, f)
+    return dic
+
+
+def get_sents(this_zip, filename):
+    all_sents = []
+    with this_zip.open(filename, "r") as fin:
+        try:
+            tree = ET.parse(fin)
+        except:
+            print("failed to parse file: {}".format(filename))
+            return None
+        root = tree.getroot()
+        sents = root.findall("s")
+        for s in sents:
+            all_sents.append(" ".join([w.text for w in s.findall("w")]))
+
+    return all_sents
+
+
+
+def copy_files(overlaps, source_dir, target_dir, lang):
+    # keys in overlaps will be english, subdicts will be key other lang, values other file
+    skipped = []
+    all_en_lines, all_other_lines = [], [] # TODO might have to do something different if can't all fit in memory...
+    with zipfile.ZipFile("en.zip") as z_en, zipfile.ZipFile("{}.zip".format(lang)) as z_other: # currently only works for bitext, easy to extend
+        all_en_files, all_other_files = set(z_en.namelist()), set(z_other.namelist())
+        for en_file in overlaps.keys():
+            # make sure all found before copying otherwise data isn't parallel
+            other_file = overlaps[en_file][lang]
+            if en_file not in all_en_files or other_file not in all_other_files:
+                skipped.append((en_file, other_file))
+                continue
+            en_sents, other_sents = get_sents(z_en, en_file), get_sents(z_other, other_file)
+            if en_sents and other_sents:
+                all_en_lines.extend(en_sents)
+                all_other_lines.extend(other_sents)
+    with open(os.path.join(target_dir, "en.txt"), "w") as en_out, \
+            open(os.path.join(target_dir, "{}.txt".format(lang)), "w") as other_out:
+        en_out.write("\n".join(all_en_lines))
+        other_out.write("\n".join(all_other_lines))
+        print("wrote {} lines of en and {} lines of {}".format(
+            len(all_en_lines), len(all_other_lines), lang))
+
+
+
+if __name__ == "__main__":
+    args = setup_argparse()
+
+    all_languages = ["zh", "eu", "ar", "fi", "id", "ta", "ru", "de", "el", "es"] # everything will be alphabetical
+    print("Working on {} languages".format(len(all_languages)))
+    for lang in all_languages:
+        lang_pair = ("{}-{}".format(*sorted([lang,"en"])))
+        # make directory for lang pair if doesn't exist
+        os.makedirs(os.path.join(args.target_dir, lang_pair), exist_ok=True)
+        overlaps = find_overlapping_files(lang_pair, target_dir=args.target_dir)
+        copy_files(overlaps, args.source_dir, args.target_dir, lang)
+
+
