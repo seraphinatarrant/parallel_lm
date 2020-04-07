927bac990f72aa6375840bcc3f3953b893301759
seraphinatarrant <serif@uw.edu>
2020-04-07 22:01:21 +0100

    adding strict filter to preserve matching bitext

--- preprocess/clean_data.py
+++ preprocess/clean_data.py
@@ -17,25 +17,69 @@ def setup_argparse():
 
     return p.parse_args()
 
-def filter_true_language(filepath, lang, output_dir, ok_langs=None):
-    if ok_langs:
-        all_langs = ok_langs.add(lang)
-    else:
-        all_langs = {lang}
+
+def strict_filter_true_language(files, langs, output_dir):
+
     pct_stripper = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
     keep_lines, cut_lines = [], []
-    print("Lang: {}".format(lang), file=sys.stderr)
+    print("Langs: {}".format(langs), file=sys.stderr)
+    with open(files[0], "r") as fin1, open(files[1], "r") as fin2:
+        for i, line1, line2 in enumerate(zip(fin1, fin2)):
+            clean_line1 = re.sub(r' +', ' ', line1.strip().translate(pct_stripper))
+            clean_line2 = re.sub(r' +', ' ', line1.strip().translate(pct_stripper))
+            predict_lang1, confidence1 = langid.classify(clean_line1)
+            predict_lang2, confidence2 = langid.classify(clean_line2)
+            # print(predict_lang, confidence)
+            if predict_lang1 not in langs[0] or predict_lang2 not in langs[1]:
+                print(
+                    "Pair:\n"
+                    "Predicted Lang {} (Conf: {:4f}) for line: {} \n"
+                    "Predicted Lang {} (Conf: {:4f}) for line: {}".format(
+                        predict_lang1, confidence1, line1.strip(),
+                        predict_lang2, confidence2, line2.strip()),
+                    file=sys.stderr)
+                cut_lines.append((line1, line2))
+            else:
+                keep_lines.append((line1, line2))
+
+    percent_kept = len(cut_lines) / (len(keep_lines) + len(cut_lines)) * 100 if keep_lines else 0
+    print("{} cleaned lines and {} cut lines ({:2f}) % were cut".format(len(keep_lines),
+                                                                        len(cut_lines),
+                                                                        percent_kept))
+    if args.output_dir:
+        out_files = []
+        for filepath in files:
+            filename = os.path.split(filepath)[1]
+            filepath = os.path.join(output_dir, filename)
+            out_files.append(filepath)
+    else:
+        out_files = files
+
+    with open(out_files[0] + "_cleaned", "w") as clean_fin1, open(out_files[1] + "_cleaned", "w") as clean_fin2:
+        clean_fin1.write("".join([l[0] for l in keep_lines]))
+        clean_fin2.write("".join([l[1] for l in keep_lines]))
+
+    with open(out_files[0] + "_cut", "w") as cut_fin1, open(out_files[1] + "_cut", "w") as cut_fin2:
+        clean_fin1.write("".join([l[0] for l in cut_lines]))
+        clean_fin2.write("".join([l[1] for l in cut_lines]))
+
+def filter_true_language(filepath, ok_langs: set, output_dir):
+
+    pct_stripper = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    keep_lines, cut_lines = [], []
+    print("Lang: {}".format(ok_langs), file=sys.stderr)
     with open(filepath, "r") as fin:
         for i, line in enumerate(tqdm(fin)):
             clean_line = re.sub(r' +', ' ', line.strip().translate(pct_stripper))
             predict_lang, confidence  = langid.classify(clean_line)
             #print(predict_lang, confidence)
-            if predict_lang not in all_langs:
+            if predict_lang not in ok_langs:
                 print("Predicted Lang {} (Conf: {:4f}) for line: {}".format(predict_lang, confidence, line.strip()),
                       file=sys.stderr)
                 cut_lines.append((i,line))
             else:
                 keep_lines.append((i,line))
+
     percent_kept = len(cut_lines)/(len(keep_lines)+len(cut_lines))*100 if keep_lines else 0
     print("{} cleaned lines and {} cut lines ({:2f}) % were cut".format(len(keep_lines),
                                                                     len(cut_lines),
@@ -49,12 +93,32 @@ def filter_true_language(filepath, lang, output_dir, ok_langs=None):
         cut_fin.write("".join(["{} {}".format(l[0], l[1]) for l in cut_lines]))
 
 
+def extract_lang(filepath: str, filetype: str):
+    if filetype == "wikimatrix":
+        this_lang = os.path.splitext(os.path.split(filepath)[1])[1][1:]  # the language is the extension, minus the .
+    else:
+        this_lang = os.path.splitext(os.path.split(filepath)[1])[0]  # filename will be lang.txt, so this grabs lang
+
+    if "_" in this_lang:  # handling for beyond standard iso code ie zh_cn which isnt in langid
+        this_lang = this_lang.split("_")[0]
+
+    return this_lang
+
+
+def expand_lang(this_lang: str, lang2ok_lang: dict) -> set:
+    if this_lang in lang2ok_lang:
+        ok_langs = lang2ok_lang[this_lang]
+    else:
+        ok_langs = {this_lang}
+    return ok_langs
+
+
 if __name__ == "__main__":
     args = setup_argparse()
 
     # some languages are sufficiently similar or sufficiently frequently mistaken for each other by classifiers that we want to consider them the same
     lang2ok_lang = {
-        "id" : {"ms"}
+        "id" : {"id", "ms"}
     }
     
     if args.data_dir:
@@ -69,17 +133,14 @@ if __name__ == "__main__":
     elif args.files:
         files = args.files
 
-    for filepath in files:
-        if args.filetype == "wikimatrix":
-            this_lang = os.path.splitext(os.path.split(filepath)[1])[1][1:] # the language is the extension, minus the .
-        else:
-            this_lang = os.path.splitext(os.path.split(filepath)[1])[0] # filename will be lang.txt, so this grabs lang
-        if "_" in this_lang: # handling for beyond standard iso code ie zh_cn which isnt in langid
-            this_lang = this_lang.split("_")[0]
+    if args.strict_filter:
+        langs = [extract_lang(f, args.filetype) for f in files]
+        ok_langs = [expand_lang(this_lang, lang2ok_lang) for this_lang in langs]
+        strict_filter_true_language(files, ok_langs, args.output_dir)
 
-        if this_lang in lang2ok_lang:
-            ok_langs = lang2ok_lang[this_lang]
-        else:
-            ok_langs = None
+    else:
+        for filepath in files:
+            this_lang = extract_lang(filepath, args.filetype)
+            ok_langs = expand_lang(this_lang, lang2ok_lang)
 
-        filter_true_language(filepath, this_lang, args.output_dir, ok_langs)
+            filter_true_language(filepath, ok_langs, args.output_dir)
